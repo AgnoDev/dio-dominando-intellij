797bd81f23bb4990fe8338d28806cfad1bb6d846
AgnoDev <agnodev87@gmail.com>
2023-03-31 16:15:23 -0300

    cria classe Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,12 +1,14 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-        System.out.println(gato);
-
+        Livro livro = new Livro("A revolução dos bichos", 150);
+//        System.out.println(gato);
+        System.out.println(livro);
         /* int a = 5;
         int b = 3;
         System.out.println("Hello, World! " + (a+b));*/
---
+++ src/br/com/dio/model/Livro.java
@@ -0,0 +1,35 @@
+package br.com.dio.model;
+
+public class Livro {
+    private String nome;
+    private int numPag;
+
+    public Livro(String nome, int numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+}
